90c71759be3861b7d3ff792eafc1af520a65e95e
Manuel Neuberger <Manuel.Neuberger@live.de>
2016-06-15 22:13:31 +0200

    Menüguide

---
+++ Assets/Scripte/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    public float ep;
+    public float lvl;
+    static GameManager gameManager;
+    void Awake()
+    {
+        if (gameManager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gameManager = this;
+            
+        }
+        else if (gameManager != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripte/Levelübergang.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Levelübergang : MonoBehaviour
+{
+
+    public void starten(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void verlassen()
+    {
+        Application.Quit();
+    }
+}
