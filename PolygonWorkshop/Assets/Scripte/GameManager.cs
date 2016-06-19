4e1c9dad6aaa26a72a9a7e523ec4d4081223edfa
Jean-Luc Etgen <jean-luc@etgen.de>
2016-06-19 14:24:29 +0200

    moved project to polygonworkshop folder

---
+++ PolygonWorkshop/Assets/Scripte/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    public double ep;
+    public double lvl;
+    public static GameManager gameManager;
+    void Awake()
+    {
+
+        if (gameManager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gameManager = this;
+            Levelübergang.Load();
+        }
+        else if (gameManager != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
